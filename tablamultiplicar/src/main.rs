6a75f55529c1948b9d45de73eb92ce441322f0f3
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-07-01 13:02:33 -0500

    Tabla de Multiplicar eligiendo un numero

--- tablamultiplicar/src/main.rs
+++ tablamultiplicar/src/main.rs
@@ -1,3 +1,14 @@
+//4. Solicite un valor y muestre la tabla de multiplicar de dicho número.
+
 fn main() {
-    println!("Hello, world!");
+    let mut _arr=[1,2,3,4,5,6,7,8,9,10,11,12];
+    let mut _res:i8=0;
+    println!("Digita un numero: ");
+    let mut num:String=String::new();
+    std::io::stdin().read_line(&mut num).unwrap();
+    let mut _num:i8=num.trim().parse().unwrap();
+    for i in _arr.iter(){
+        _res=_num*i;
+        println!("{i} x {_num} = {_res}");
+    }
 }
