72e0022f6a6231a87bae8ad7c074725d4c46f07f
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-07-01 12:26:32 -0500

    Sumatoria y promedio final

--- promedio/src/main.rs
+++ promedio/src/main.rs
@@ -1,3 +1,27 @@
+//3. Diseñe un programa que solicite 10 números enteros, calcule el promedio de los mismos.
+
 fn main() {
-    println!("Hello, world!");
+    //Declaraciones
+    let mut _arr:Vec<i32>=Vec::new();
+    let mut i=0;
+    let mut sum:i32=0;
+    //ciclo
+    while i<=9{
+        println!("Digita un numero {i}: ");
+        let mut numero:String=String::new();
+        std::io::stdin().read_line(&mut numero).unwrap();
+        let mut _numero:i32=numero.trim().parse().unwrap();
+        _arr.push(_numero);
+        i+=1;
+    }println!("{:?}",_arr);
+    
+    for j in _arr.iter(){
+       sum+=j;// sumatoria que es igual a : suma=suma + j
+    }
+    println!("Sumatoria Final: {sum}");
+    println!("Promedio Final: {:?}",sum/10);
+   
+
+    
 }
+ 
